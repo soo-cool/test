fe5c53eff4ba56cad4028a0f39d09d8bb67396d5
yeyeye <[Email@example.com](mailto:Email@example.com)>
2020-01-06 10:13:05 +0800

    first

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+from user_dialog import UserDialog
+
+class Connection:
+    def __init__(self):
+        UserDialog.Get_User_Input_Id()
+        self.host = UserDialog._ip
+        self.port = UserDialog._port
+        self.sock = socket.socket()
+        self.sock.connect(self.host,self.port)
+
+
+if __name__ == '__main__':
+    conn = Connection
+
+    print('start')
